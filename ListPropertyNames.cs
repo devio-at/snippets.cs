d5a0d2bd03ffcad2f3247fcff7c4bf844e9ce2b1
herbert <herbert@oppolzer.at>
2021-08-22 10:52:50 +0200

    ListPropertyNames.cs

---
+++ ListPropertyNames.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ListPropertyNames
+{
+    // database entities
+
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+
+    class Program
+    {
+        public static string GetPropertyName<T, TP>(Expression<Func<T, TP>> expression)
+        {
+            if (expression.Body is UnaryExpression { NodeType: ExpressionType.Convert } unex)
+            {
+                var ex = unex.Operand;
+                var mex = (MemberExpression)ex;
+                return mex.Member.Name;
+            }
+
+            var memberExpression = (MemberExpression)expression.Body;
+            return memberExpression.Member.Name;
+        }
+
+        public static string EnumerateColumns<T>(string tablePrefix, string columnPrefix, params Expression<Func<T, object>>[] columns)
+        {
+            return string.Join(", ", columns.Select(c =>
+                (tablePrefix != null ? (tablePrefix + ".") : "") +
+                GetPropertyName(c) +
+                (columnPrefix != null ? (" AS " + columnPrefix + GetPropertyName(c)) : "")));
+        }
+
+        static void Main(string[] args)
+        {
+            var actualSql = "SELECT "
+                            + EnumerateColumns<Supplier>("s", "Supplier", s => s.Id, s => s.Name)
+                            + ", "
+                            + EnumerateColumns<Address>("a", "Address", a => a.ZipCode, a => a.City, a => a.City)
+                            + @"
+FROM " + nameof(Supplier) + @" s
+INNER JOIN " + nameof(Address) + " a ON s." + nameof(Supplier.AddressId) + " = a." + nameof(Addresse.Id);
+
+            Console.WriteLine(actualSql);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-Some C# Snippets from my blog.
+## Some C# Snippets from my blog.
 
-* NHibernateStringComparison.cs
+| File | Blog |
+| ---- | ---- |
+| NHibernateStringComparison.cs | [NHibernate String Comparison](https://devio.wordpress.com/2012/08/20/nhibernate-string-comparison/) (2012) |
+| HtmlHelper.For.cs | implement `@Html.For()` to support custom form model class (2011) |
+| ListPropertyNames.cs |  (2021) |
 
-[NHibernate String Comparison](https://devio.wordpress.com/2012/08/20/nhibernate-string-comparison/)
-  
-* HtmlHelper.For.cs
+### Html.For.cs
 
-implement `@Html.For()` to support custom form model class
-  
 [Getting an HtmlHelper for an alternate model type](https://daveaglick.com/posts/getting-an-htmlhelper-for-an-alternate-model-type)
         
 [ASP Net MVC type-safe HTML TextBoxFor with different output model](http://stackoverflow.com/questions/1321254/asp-net-mvc-typesafe-html-textboxfor-with-different-outputmodel)
